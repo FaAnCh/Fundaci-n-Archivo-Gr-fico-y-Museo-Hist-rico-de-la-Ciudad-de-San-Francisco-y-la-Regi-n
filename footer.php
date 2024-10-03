5ba379073d2e87d5ced0a84b01aea925eaaf578f
FaAnCh <99348044+FaAnCh@users.noreply.github.com>
2024-10-03 09:34:29 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+        <hr />
+        <div class="footer-content">
+          <div class="logo-section">
+            <img src="assets\fundacionAGM.png" alt="fundacion AGM" />
+          </div>
+          <div class="contact-info">
+            <p>
+              Avenida de la Universidad 271<br />
+              2400 San Francisco (Córdoba)<br /><br />
+              Tel. 00+54+3564-15608752<br /><br />
+              archivograficoymuseo@yahoo.com.ar<br /><br />
+              Facebook / Archivo Gráfico
+            </p>
+          </div>
+          <div class="sponsors">
+            <p>
+              Nos apoyan:<br />
+              Micron, Akron, Axion, Compañía de Seguros El Norte, Macoser.
+            </p>
+            <br />
+            <p>
+              Medios de comunicación que nos respaldan:<br />
+              Diario La Voz de San Justo, AM 1050, RadioCanal, Canal 4 y FM
+              Contacto; El Periódico, Radio Estación, El Tiempo.
+            </p>
+          </div>
+        </div>
+        <div class="copyright">
+          © Escuela ProA Técnica
+        </div>
+      </footer>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+      <nav class="navbar">
+        <div class="navbar-logo">
+          <a href="index.php"
+            ><img src="assets/fundacionAGM.png" alt="Logo AGM"
+          /></a>
+        </div>
+        <ul class="navbar-menu">
+          <li class="navbar-item"><a href="index.php">Inicio</a></li>
+          <li class="navbar-item"><a href="museo.php">Nosotros</a></li>
+          <li class="navbar-item"><a href="#">Archivo</a></li>
+          <li class="navbar-item"><a href="visitas.php">Visitas</a></li>
+          <li class="navbar-item"><a href="contacto.php">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+</body>
+</html>
