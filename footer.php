0a3b8b3778f3365a64cab3e2a3d59a5aa1954ca3
Jonathan Medrano <59778931+Jonathan-Medrano@users.noreply.github.com>
2024-10-08 09:44:00 -0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Descubre la Fundación Archivo Gráfico y Museo Histórico de San Francisco y la Región. Conoce nuestras exposiciones, programa de visitas escolares y cómo ponerte en contacto con nosotros para más información.">
     <meta name="keywords" content="museo, Fundación Archivo Gráfico, San Francisco, exposiciones, visitas escolares, historia, cultura, arte, patrimonio, educación, contacto">
     <link rel="icon" type="image/png" href="assets/fundacionAGM.png">
-
+    <link rel="stylesheet" href="footer.css" />
 </head>
 <body>
     <footer>
--- nav.php
+++ nav.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="nav.css">
     <meta name="description" content="Descubre la Fundación Archivo Gráfico y Museo Histórico de San Francisco y la Región. Conoce nuestras exposiciones, programa de visitas escolares y cómo ponerte en contacto con nosotros para más información.">
     <meta name="keywords" content="museo, Fundación Archivo Gráfico, San Francisco, exposiciones, visitas escolares, historia, cultura, arte, patrimonio, educación, contacto">
     <link rel="icon" type="image/png" href="assets/fundacionAGM.png">
-
 </head>
 <body>
 <header>
