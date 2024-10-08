8acea5f85b1e74c5ce1769bd2005a73553683e53
FaAnCh <99348044+FaAnCh@users.noreply.github.com>
2024-10-08 09:07:34 -0300

    Add files via upload

--- nav.php
+++ nav.php
@@ -1,54 +1,37 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <meta name="description" content="Descubre la Fundación Archivo Gráfico y Museo Histórico de San Francisco y la Región. Conoce nuestras exposiciones, programa de visitas escolares y cómo ponerte en contacto con nosotros para más información.">
+    <meta name="keywords" content="museo, Fundación Archivo Gráfico, San Francisco, exposiciones, visitas escolares, historia, cultura, arte, patrimonio, educación, contacto">
+    <link rel="icon" type="image/png" href="assets/fundacionAGM.png">
+
 </head>
 <body>
 <header>
-      <nav class="navbar">
+    <nav class="navbar">
         <div class="navbar-logo">
-          <a href="index.php"
-            ><img src="assets/fundacionAGM.png" alt="Logo AGM"
-          /></a>
+            <a href="index.php">
+                <img src="assets/fundacionAGM.png" alt="Logo AGM" />
+            </a>
         </div>
-        <ul class="navbar-menu">
-          <li class="navbar-item"><a href="index.php">Inicio</a></li>
-          <li class="navbar-item"><a href="museo.php">Nosotros</a></li>
-          <li class="navbar-item"><a href="#">Archivo</a></li>
-          <li class="navbar-item"><a href="visitas.php">Visitas</a></li>
-          <li class="navbar-item"><a href="contacto.php">Contacto</a></li>
-        </ul>
-      </nav>
-    </header>
-</body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<header>
-      <nav class="navbar">
-        <div class="navbar-logo">
-          <a href="index.php"
-            ><img src="assets/fundacionAGM.png" alt="Logo AGM"
-          /></a>
+        <div class="navbar-toggle" id="navbar-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
-        <ul class="navbar-menu">
-          <li class="navbar-item"><a href="index.php">Inicio</a></li>
-          <li class="navbar-item"><a href="museo.php">Nosotros</a></li>
-          <li class="navbar-item"><a href="#">Archivo</a></li>
-          <li class="navbar-item"><a href="visitas.php">Visitas</a></li>
-          <li class="navbar-item"><a href="contacto.php">Contacto</a></li>
+        <ul class="navbar-menu" id="navbar-menu">
+            <li class="navbar-item"><a href="index.php">Inicio</a></li>
+            <li class="navbar-item"><a href="museo.php">Nosotros</a></li>
+            <li class="navbar-item"><a href="archivo.php">Archivo</a></li>
+            <li class="navbar-item"><a href="visitas.php">Visitas</a></li>
+            <li class="navbar-item"><a href="contacto.php">Contacto</a></li>
         </ul>
-      </nav>
-    </header>
+    </nav>
+</header>
+<script src="burger.js"></script>
 </body>
->>>>>>> 94a5b033a0ece03d5e5e5704cff62269056ae1ee
 </html>
--- visitas.php
+++ visitas.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -6,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles.css" />
     <link rel="stylesheet" href="visitas.css" />
-    <title>Visitas</title>
+    <title>Visitas - Archivo Gráfico</title>
+    <meta name="description" content="Descubre la Fundación Archivo Gráfico y Museo Histórico de San Francisco y la Región. Conoce nuestras exposiciones, programa de visitas escolares y cómo ponerte en contacto con nosotros para más información.">
+    <meta name="keywords" content="museo, Fundación Archivo Gráfico, San Francisco, exposiciones, visitas escolares, historia, cultura, arte, patrimonio, educación, contacto">
+    <link rel="icon" type="image/png" href="assets/fundacionAGM.png">
+
   </head>
   <body>
   <?php include "nav.php"?>
@@ -47,53 +50,3 @@
 
   </body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="styles.css" />
-    <link rel="stylesheet" href="visitas.css" />
-    <title>Visitas</title>
-  </head>
-  <body>
-  <?php include "nav.php"?>
-
-
-    <main>
-      <section class="gallery">
-        <h1>Visitas</h1>
-        <div class="gallery-grid">
-          <div class="gallery-item">
-            <img src="assets/img1.jpg" alt="Descripción de la imagen 1" />
-          </div>
-          <div class="gallery-item">
-            <img src="assets/img2.jpg" alt="Descripción de la imagen 2" />
-          </div>
-          <div class="gallery-item">
-            <img src="assets/img3.jpg" alt="Descripción de la imagen 3" />
-          </div>
-          <div class="gallery-item">
-            <img src="assets/img4.jpg" alt="Descripción de la imagen 4" />
-          </div>
-          <div class="gallery-item">
-            <img src="assets/img5.jpg" alt="Descripción de la imagen 5" />
-          </div>
-          <div class="gallery-item">
-            <img src="assets/img6.jpg" alt="Descripción de la imagen 6" />
-          </div>
-          <div class="gallery-item">
-            <img src="assets/img7.jpg" alt="Descripción de la imagen 7" />
-          </div>
-          <div class="gallery-item">
-            <img src="assets/img8.jpg" alt="Descripción de la imagen 8" />
-          </div>
-        </div>
-      </section>
-    </main>
-    <?php include "footer.php"?>
-
-  </body>
-</html>
->>>>>>> 94a5b033a0ece03d5e5e5704cff62269056ae1ee
