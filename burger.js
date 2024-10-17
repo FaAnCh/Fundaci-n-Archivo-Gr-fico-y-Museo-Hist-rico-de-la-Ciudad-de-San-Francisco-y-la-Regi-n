01e7c63fdf98945dff62444529d38aae11e25f40
FaAnCh <99348044+FaAnCh@users.noreply.github.com>
2024-10-17 10:11:19 -0300

    Delete burger.js

--- burger.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById('navbar-toggle').addEventListener('click', function() {
-    const menu = document.getElementById('navbar-menu');
-    menu.classList.toggle('active'); // Alternar la clase active
-});
